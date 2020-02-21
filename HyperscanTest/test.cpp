b3747247c1e934637bd60958de1a5a708357a967
Yang Luo <hsluoyz@qq.com>
2020-02-21 17:53:00 +0800

    Use regex string array.

--- HyperscanTest/test.cpp
+++ HyperscanTest/test.cpp
@@ -1,30 +1,41 @@
 #include "pch.h"
 #include "hs.h"
 
-hs_database_t* database;
+hs_database_t* database = NULL;
 hs_scratch_t* scratch = NULL;
 bool gIsMatched = false;
 
-char* regexBrowserOpera = "(?:Chrome).*OPR/\\d+\\.\\d+\\.\\d+|Opera/";
-char* regexBrowserEdge = "EdgiOS|Edge/\\d+\\.\\d+";
-char* regexBrowserChrome = "CriOS|Chromium|Chrome/\\d+\\.\\d+(?:\\.\\d+)?(?:\\.\\d+)?";
-char* regexBrowserFirefox = "FxiOS|Firefox/";
-char* regexBrowserSafari = "Safari/";
-char* regexBrowserIe = "Trident/|[MS]?IE \\d+\\.\\d+";
+char* regexBrowserArray[] = {
+    "(?:Chrome).*OPR/\\d+\\.\\d+\\.\\d+|Opera/",
+    "EdgiOS|Edge/\\d+\\.\\d+",
+    "CriOS|Chromium|Chrome/\\d+\\.\\d+(?:\\.\\d+)?(?:\\.\\d+)?",
+    "FxiOS|Firefox/",
+    "Safari/",
+    "Trident/|[MS]?IE \\d+\\.\\d+"
+};
 
-char* regexOsWindows = "Windows|Win 9x|WinNT|Windows ?CE|Win ?(?:95|98|3.1|NT 4.0|ME|2000)|Windows ?(?:95|98|3.1|NT 4.0|ME|2000)|Win16|Win32";
-char* regexOsAndroid = "Android|Silk-Accelerated";
-char* regexOsLinux = "[Dd]ebian|Linux|linux|Fedora|Red Hat|PCLinuxOS|Puppy|Ubuntu|Kindle|Bada|Lubuntu|BackTrack|Slackware|(?:Free|Open|Net|\\b)BSD[/ ]|Ubuntu|Kubuntu|Arch Linux|CentOS|Slackware|Gentoo|openSUSE|SUSE|Red Hat|Fedora|PCLinuxOS|Mageia|(?:Free|Open|Net|\b)BSD|freebsd";
-char* regexOsIos = "iPhone|iPad|iPod|like Mac OS X";
-char* regexOsMacOs = "(?:Mac[ +]?|; )OS[ +]X|Mac OS X|Macintosh|Darwin";
+char* regexOsArray[] = {
+    "Windows|Win 9x|WinNT|Windows ?CE|Win ?(?:95|98|3.1|NT 4.0|ME|2000)|Windows ?(?:95|98|3.1|NT 4.0|ME|2000)|Win16|Win32",
+    "Android|Silk-Accelerated",
+    "[Dd]ebian|Linux|linux|Fedora|Red Hat|PCLinuxOS|Puppy|Ubuntu|Kindle|Bada|Lubuntu|BackTrack|Slackware|(?:Free|Open|Net|\\b)BSD[/ ]|Ubuntu|Kubuntu|Arch Linux|CentOS|Slackware|Gentoo|openSUSE|SUSE|Red Hat|Fedora|PCLinuxOS|Mageia|(?:Free|Open|Net|\b)BSD|freebsd",
+    "iPhone|iPad|iPod|like Mac OS X",
+    "(?:Mac[ +]?|; )OS[ +]X|Mac OS X|Macintosh|Darwin"
+};
 
-char* regexDeviceSmartTv = "TV|tvOS";
-char* regexDeviceTablet = "iPad|Tablet|Silk|Kindle|; ARM; Trident/6\\.0; Touch[\\);]";
-char* regexDeviceSmartphone = "iPhone|Android|KAIOS/|Windows Phone OS";
+char* regexDeviceArray[] = {
+    "TV|tvOS",
+    "iPad|Tablet|Silk|Kindle|; ARM; Trident/6\\.0; Touch[\\);]",
+    "iPhone|Android|KAIOS/|Windows Phone OS"
+};
 
 enum BrowserName {kBrowserOpera, kBrowserEdge, kBrowserChrome, kBrowserFirefox, kBrowserSafari, kBrowserIe, kBrowserOther};
+char* browserNames[] = {"Opera", "Edge", "Chrome", "Firefox", "Safari", "IE", "Other"};
+
 enum OsName {kOsWindows, kOsAndroid, kOsLinux, kOsIos, kOsMacOs, kOsOther};
+char* osNames[] = { "Windows", "Android", "Linux", "iOS", "macOS", "Other" };
+
 enum DeviceName {kDeviceSmartTv, kDeviceTablet, kDeviceSmartphone, kDeviceOther};
+char* deviceNames[] = { "SmartTV", "Tablet", "Smartphone", "Other" };
 
 // Hyperscan Example Code
 // https://github.com/intel/hyperscan/tree/master/examples
@@ -45,16 +56,38 @@ int initPattern(char* pattern) {
     return 0;
 }
 
+void releasePattern() {
+    hs_free_scratch(scratch);
+    scratch = NULL;
+    hs_free_database(database);
+    database = NULL;
+}
+
+// void initAllPatterns() {
+//     for (int i = 0; i < kBrowserOther; i++) {
+//         initPattern(regexBrowserArray[i]);
+//     }
+//
+//     for (int i = 0; i < kOsOther; i++) {
+//         initPattern(regexOsArray[i]);
+//     }
+//
+//     for (int i = 0; i < kDeviceOther; i++) {
+//         initPattern(regexDeviceArray[i]);
+//     }
+// }
+
 static int eventHandler(unsigned int id, unsigned long long from,
     unsigned long long to, unsigned int flags, void* ctx) {
     gIsMatched = true;
-    printf("Match for pattern \"%s\" at offset %llu\n", (char*)ctx, to);
+    // printf("Match for pattern \"%s\" at offset %llu\n", (char*)ctx, to);
     return 0;
 }
 
 bool isMatched(char* pattern, char* userAgent) {
     gIsMatched = false;
 
+    initPattern(pattern);
     if (hs_scan(database, userAgent, strlen(userAgent), 0, scratch, eventHandler,
         pattern) != HS_SUCCESS) {
         fprintf(stderr, "ERROR: Unable to scan input buffer. Exiting.\n");
@@ -63,62 +96,50 @@ bool isMatched(char* pattern, char* userAgent) {
         hs_free_database(database);
         return false;
     }
+    releasePattern();
 
     return gIsMatched;
 }
 
 BrowserName checkBrowser(char* userAgent) {
-    if (isMatched(regexBrowserOpera, userAgent)) {
-        return kBrowserOpera;
-    } else if (isMatched(regexBrowserEdge, userAgent)) {
-        return kBrowserEdge;
-    } else if (isMatched(regexBrowserChrome, userAgent)) {
-        return kBrowserChrome;
-    } else if (isMatched(regexBrowserFirefox, userAgent)) {
-        return kBrowserFirefox;
-    } else if (isMatched(regexBrowserSafari, userAgent)) {
-        return kBrowserSafari;
-    } else if (isMatched(regexBrowserIe, userAgent)) {
-        return kBrowserIe;
-    } else {
-        return kBrowserOther;
+    for (int i = 0; i < kBrowserOther; i++) {
+        if (isMatched(regexBrowserArray[i], userAgent)) {
+            return (BrowserName) i;
+        }
     }
+
+    return kBrowserOther;
 }
 
 OsName checkOs(char* userAgent) {
-    if (isMatched(regexOsWindows, userAgent)) {
-        return kOsWindows;
-    } else if (isMatched(regexOsAndroid, userAgent)) {
-        return kOsAndroid;
-    } else if (isMatched(regexOsLinux, userAgent)) {
-        return kOsLinux;
-    } else if (isMatched(regexOsIos, userAgent)) {
-        return kOsIos;
-    } else if (isMatched(regexOsMacOs, userAgent)) {
-        return kOsMacOs;
-    } else {
-        return kOsOther;
+    for (int i = 0; i < kOsOther; i++) {
+        if (isMatched(regexOsArray[i], userAgent)) {
+            return (OsName)i;
+        }
     }
+
+    return kOsOther;
 }
 
 DeviceName checkDevice(char* userAgent) {
-    if (isMatched(regexDeviceSmartTv, userAgent)) {
-        return kDeviceSmartTv;
-    } else if (isMatched(regexDeviceTablet, userAgent)) {
-        return kDeviceTablet;
-    } else if (isMatched(regexDeviceSmartphone, userAgent)) {
-        return kDeviceSmartphone;
-    } else {
-        return kDeviceOther;
+    for (int i = 0; i < kDeviceOther; i++) {
+        if (isMatched(regexDeviceArray[i], userAgent)) {
+            return (DeviceName)i;
+        }
     }
+
+    return kDeviceOther;
 }
 
 void testMatch(char* userAgent, bool res) {
-    // init(pattern);
+    // initAllPatterns();
+
     BrowserName browserName = checkBrowser(userAgent);
     OsName osName = checkOs(userAgent);
     DeviceName deviceName = checkDevice(userAgent);
-    printf("(%s, %s, %s)\n", browserName, osName, deviceName);
+
+    printf("(%s, %s, %s)\n", browserNames[browserName], osNames[osName], deviceNames[deviceName]);
+
     // EXPECT_EQ(myRes, res);
 }
 
